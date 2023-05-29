627f60356dc8ed5d43fe29209d0d3f8a977a9ce7
de-esteves <guilherme.esteves@grupoboticario.com.br>
2023-05-28 21:59:32 -0300

    fix: exectuando o shell script no client anter de subir

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_function" "lambda_decompress" {
-  filename      = "../functions/fn_extract_rais.zip"
+  filename      = "lambda_function_payload.zip"
   function_name = "${var.prefix}_extract_rais"
   role          = aws_iam_role.lambda_decompress.arn
   handler       = "lambda_function.handler"
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  source_code_hash = filebase64sha256("../functions/fn_extract_rais.zip")
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
   runtime          = "python3.8"
   timeout          = 900
   memory_size      = 1024
